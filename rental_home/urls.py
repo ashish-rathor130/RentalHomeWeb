184fc6a9eb619eb5f74006241237c6c2caa5d824
Ashish Rathor <anvdgroup@gmail.com>
2026-05-03 15:49:01 +0530

    Update urls.py

--- rental_home/urls.py
+++ rental_home/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 # urls.py (main project)
-from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +27,5 @@ urlpatterns = [
     path('',include('home.urls')),
     path('houses/',include('properties.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
